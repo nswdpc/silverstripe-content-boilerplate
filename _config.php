52563c5d8e5f63dc2c295ef567485a17c1adbfd0
Mark Taylor <tardinha@gmail.com>
2021-08-30 11:51:12 +1000

    Tidy up tinymce config

--- _config.php
+++ _config.php
@@ -6,9 +6,6 @@ TinyMCEConfig::get('cms')->setOption('height', 420);
 TinyMCEConfig::get('cms')->setOption('min_height', 420);
 TinyMCEConfig::get('cms')->setOption('max_height', 960);
 
-TinyMCEConfig::get('cms')->setOption('menubar', ['view','insert']);
-TinyMCEConfig::get('cms')->setOption('toolbar', ['fullscreen','anchor']);
-
 TinyMCEConfig::get('cms')->removeButtons('underline', 'alignjustify');
 
 TinyMCEConfig::get('cms')->enablePlugins(
@@ -33,7 +30,6 @@ TinyMCEConfig::get('cms')->addButtonsToLine(1, '|', 'visualblocks');
 TinyMCEConfig::get('cms')->addButtonsToLine(2, '|', 'fullscreen');
 
 TinyMCEConfig::get('cms')->insertButtonsAfter('formatselect', 'styleselect');
-// TinyMCEConfig::get('cms')->insertButtonsAfter('unlink', 'anchor');
 
 TinyMCEConfig::get('cms')->setOptions([
     'extended_valid_elements' => 'ol[start]',
