def9f4f2d32daa26471fa3fbb301f614dfc476f8
James (PD) <69664712+JamesDPC@users.noreply.github.com>
2025-10-28 15:56:23 +1100

    Wrap config in try-catch

--- _config.php
+++ _config.php
@@ -1,31 +1,35 @@
 <?php
 use SilverStripe\Forms\HTMLEditor\TinyMCEConfig;
 
-TinyMCEConfig::get('cms')->setOption('min_height', 420);
-TinyMCEConfig::get('cms')->setOption('max_height', 600);
+try {
+    
+    TinyMCEConfig::get('cms')->setOption('min_height', 420);
+    TinyMCEConfig::get('cms')->setOption('max_height', 600);
 
-TinyMCEConfig::get('cms')->removeButtons('underline', 'alignjustify', 'blocks');
+    TinyMCEConfig::get('cms')->removeButtons('underline', 'alignjustify', 'blocks');
 
-TinyMCEConfig::get('cms')->enablePlugins(
-    'autoresize',
-    'fullscreen',
-    'visualblocks',
-    'anchor',
-    'wordcount',
-    'lists',
-    'advlist'
-);
+    TinyMCEConfig::get('cms')->enablePlugins(
+        'autoresize',
+        'fullscreen',
+        'visualblocks',
+        'anchor',
+        'wordcount',
+        'lists',
+        'advlist'
+    );
 
-TinyMCEConfig::get('cms')->insertButtonsAfter(
-    'removeformat',
-    '|',
-    'superscript',
-    'subscript',
-    'blockquote',
-    'hr'
-);
-TinyMCEConfig::get('cms')->addButtonsToLine(2, '|', 'fullscreen');
+    TinyMCEConfig::get('cms')->insertButtonsAfter(
+        'removeformat',
+        '|',
+        'superscript',
+        'subscript',
+        'blockquote',
+        'hr'
+    );
+    TinyMCEConfig::get('cms')->addButtonsToLine(2, '|', 'fullscreen');
 
-TinyMCEConfig::get('cms')->insertButtonsBefore('pastetext', 'styles');
+    TinyMCEConfig::get('cms')->insertButtonsBefore('pastetext', 'styles');
 
-TinyMCEConfig::get('cms')->setOptions(['extended_valid_elements' => 'ol[start]',]);
+    TinyMCEConfig::get('cms')->setOptions(['extended_valid_elements' => 'ol[start]',]);
+
+} catch (\Exception $exception) {}
