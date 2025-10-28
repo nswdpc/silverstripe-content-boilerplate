402bf34e36da41aa7e8c083e4ea6d0fb611bdcf2
James (PD) <69664712+JamesDPC@users.noreply.github.com>
2025-10-28 16:26:42 +1100

    Apply a default style_formats if one has not been applied

--- _config.php
+++ _config.php
@@ -2,7 +2,7 @@
 use SilverStripe\Forms\HTMLEditor\TinyMCEConfig;
 
 try {
-    
+
     TinyMCEConfig::get('cms')->setOption('min_height', 420);
     TinyMCEConfig::get('cms')->setOption('max_height', 600);
 
@@ -31,5 +31,67 @@ try {
     TinyMCEConfig::get('cms')->insertButtonsBefore('pastetext', 'styles');
 
     TinyMCEConfig::get('cms')->setOptions(['extended_valid_elements' => 'ol[start]',]);
+
+    if(is_null(TinyMCEConfig::get('cms')->getOption('style_formats'))) {
+        TinyMCEConfig::get('cms')->setOptions([
+            'importcss_append' => true,
+            'style_formats' => [
+                [
+                    'title' => 'Text',
+                    'items' => [
+                        [
+                            'title' => 'Paragraph',
+                            'block' => 'p',
+                            'attributes' => [
+                                'class' => ''
+                            ]
+                        ],
+                        [
+                            'title' => 'Heading 1',
+                            'block' => 'h1',
+                            'attributes' => [
+                                'class' => ''
+                            ]
+                        ],
+                        [
+                            'title' => 'Heading 2',
+                            'block' => 'h2',
+                            'attributes' => [
+                                'class' => ''
+                            ]
+                        ],
+                        [
+                            'title' => 'Heading 3',
+                            'block' => 'h3',
+                            'attributes' => [
+                                'class' => ''
+                            ]
+                        ],
+                        [
+                            'title' => 'Heading 4',
+                            'block' => 'h4',
+                            'attributes' => [
+                                'class' => ''
+                            ]
+                        ],
+                        [
+                            'title' => 'Heading 5',
+                            'block' => 'h5',
+                            'attributes' => [
+                                'class' => ''
+                            ]
+                        ],
+                        [
+                            'title' => 'Heading 6',
+                            'block' => 'h6',
+                            'attributes' => [
+                                'class' => ''
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+    }
 
 } catch (\Exception $exception) {}
